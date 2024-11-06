5aa4ec1064f2cd2dfe4807e1bddbf09e510046d2
Jorge <jorge_luis.2008@hotmail.com>
2024-11-05 22:24:38 -0600

    Merge branch 'qa' into main

--- src/app/tests/playwright/example.spec.js
+++ src/app/tests/playwright/example.spec.js
@@ -1,8 +1,11 @@
 // example.spec.js
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
 
+dotenv.config();
 test('visit the main page', async ({ page }) => {
-    await page.goto('http://localhost:3000'); // Navigate to the website
+    const port = process.env.NEXT_PORT;
+    await page.goto(`http://localhost:${port}`); // Navigate to the website
     const title = await page.title(); // Get the page title
     expect(title).toBe('Create Next App'); // Check if the title matches the expected title
 });
