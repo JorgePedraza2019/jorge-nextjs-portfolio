2fe1a7abaa791bcde1902ff30a8b33b142ac8191
Jorge <jorge_luis.2008@hotmail.com>
2025-12-30 16:04:17 -0600

    ci changes 15

--- frontend/src/app/tests/playwright/example.spec.js
+++ frontend/src/app/tests/playwright/example.spec.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 test('visit the main page', async ({ page }) => {
     const port = process.env.NEXT_PORT;
-    await page.goto(`http://localhost:${port}`); // Navigate to the website
+    await page.goto(`http://localhost:${port}/en`); // Navigate to the website
     const title = await page.title(); // Get the page title
-    expect(title).toBe('Create Next App'); // Check if the title matches the expected title
+    expect(title).toBe("Jorge's Portfolio"); // Check if the title matches the expected title
 });
